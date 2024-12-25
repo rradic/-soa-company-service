a07af13dd54d0ddf65073a958997bdb200abdf6c
rradic <radovan.radic@student.um.si>
2024-12-25 01:57:12 +0100

    Init commit

---
+++ src/test/java/org/soa/companyService/ComapnyService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soa.companyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComapnyService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
